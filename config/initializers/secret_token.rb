d3977c9178221293721a4a1240571bedace2a4ff
Gen Takahashi <gendosu@gmail.com>
2013-09-08 15:46:17 +0900

    初期コミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PasswordGenerator::Application.config.secret_key_base = '719013f47733a3b5cb73eb112beb1a3ebc1fc8467c7e2519059e1f8c95bfc91c91ed66af385950b7c1284529105b1e43da7067c00b7a7206560006c8348cd8e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PasswordGenerator::Application.config.session_store :cookie_store, key: '_password_generator_session'
