18da9910126715be40ff1c73a363527af875d326
Gen Takahashi <gendosu@gmail.com>
2013-09-08 23:22:57 +0900

    パスワード文字数を指定できるように修正
    改行追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,7 @@ class TopController < ApplicationController
   class PasswordOptions
     include ActiveModel::Model
 
+    attr_accessor :password_size
     attr_accessor :include_sharp
   end
 
@@ -36,7 +37,7 @@ class TopController < ApplicationController
     
     if request.post?
       @password_result = PasswordResult.new
-      @password_result.password = @@rundom_generator.first(10).join
+      @password_result.password = @@rundom_generator.first(@password_options.password_size.to_i).join
     end
   end
 end
