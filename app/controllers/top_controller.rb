ed01bc24b952a20972d2e9535d717eb9836e5082
Gen Takahashi <gendosu@gmail.com>
2013-11-13 23:31:57 +0900

    とりあえずコントローラをコミット

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
-  
+
+  # パスワード生成用のフォーム項目
   class PasswordOptions
     include ActiveModel::Model
 
@@ -7,37 +8,22 @@ class TopController < ApplicationController
     attr_accessor :include_sharp
   end
 
+  # パスワード生成後の結果
   class PasswordResult
     include ActiveModel::Model
 
     attr_accessor :password
   end
   
-  # = ランダム文字生成器(Ruby v2.0)
-  #
-  # eachで回す際に、サイズを気にせずに無限に回せます。
-  # 無限に回すと止まらないので、first(10)という感じに制限をつけてやるといい感じです。
-  #
-  # === 使い方
-  # rundom_generator.first(10).each do |a|
-  # p a
-  # end
-  #
-  @@rundom_generator = Enumerator.new{|yielder|
-    # キーに出現する文字を定義
-    key_chars = (('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a)
-    random = Random.new
-    1.upto(Float::INFINITY) do |n|
-      yielder << key_chars[random.rand(key_chars.size)]
-    end
-  }
-
+  # === トップページ
   def index
+    # パスワード生成用フォームの項目インスタンスを生成
     @password_options = PasswordOptions.new(params[:password_options])
-    
+
+    # リクエストがポストだったら、パスワードを生成して返す
     if request.post?
       @password_result = PasswordResult.new
-      @password_result.password = @@rundom_generator.first(@password_options.password_size.to_i).join
+      @password_result.password = Password.generate(@password_options.password_size.to_i)
     end
   end
 end
